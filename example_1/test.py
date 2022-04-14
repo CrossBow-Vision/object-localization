f15c3bbf1db0095fb2954fcfc85ce557ea884d55
CrossBow-Vision <97107240+CrossBow-Vision@users.noreply.github.com>
2022-04-14 21:36:56 +0530

    Update test.py

--- example_1/test.py
+++ example_1/test.py
@@ -1,6 +1,7 @@
 import glob
 import cv2
 import numpy as np
+import tensorflow as tf
 
 from train import create_model, IMAGE_SIZE
 from keras.applications.mobilenetv2 import preprocess_input
@@ -12,9 +13,12 @@ def main():
     model = create_model()
     model.load_weights(WEIGHTS_FILE)
 
-    for img in glob.glob(IMAGES):
-
-        region = model.predict(x=np.array([img]))[0]
+    for filename in glob.glob(IMAGES):
+        
+        unscaled_img = cv2.imread(filename)        
+        img_scaled = preprocess_input(np.array(unscaled_img, dtype=np.float32))
+        
+        region = model.predict(x=np.array([img_scaled]))[0]
 
         x0 = int(region[0] )
         y0 = int(region[1] )
@@ -22,8 +26,8 @@ def main():
         x1 = int((region[0] + region[2]) )
         y1 = int((region[1] + region[3]) )
 
-        cv2.rectangle(unscaled, (x0, y0), (x1, y1), (0, 0, 255), 1)
-        cv2.imshow("image", unscaled)
+        cv2.rectangle(unscaled_img, (x0, y0), (x1, y1), (0, 0, 255), 1)
+        cv2.imshow("image", unscaled_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
